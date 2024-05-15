558e89366b9a403849feeeef3155b668c087ba81
valdano345 <valdanoesnaidar@gmail.com>
2024-05-15 09:24:40 +0700

    fix tutor6

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -3,13 +3,17 @@ import { NextRequest, NextResponse } from "next/server";
 const data = [
   {
     id: 1,
-    name: "Sepatu Baru",
+    title: "Sepatu Baru",
     price: 100000,
+    image:
+      "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_0.8/h_486,c_limit/4693a962-0ea1-4677-b5fd-eeed550ab4d3/air-jordan-1-low-shoes-6Q1tFM.png",
   },
   {
     id: 2,
-    name: "Sepatu Baru Dongg",
+    title: "Sepatu Baru Dongg",
     price: 1000000,
+    image:
+      "https://static.nike.com/a/images/q_auto:eco/t_product_v1/f_auto/dpr_0.8/h_486,c_limit/4693a962-0ea1-4677-b5fd-eeed550ab4d3/air-jordan-1-low-shoes-6Q1tFM.png",
   },
 ];
 
