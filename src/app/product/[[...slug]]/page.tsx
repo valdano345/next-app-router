19aeb40285df93474cc7d0ae3aa7f97d73ec19d0
valdano345 <valdanoesnaidar@gmail.com>
2024-05-15 13:57:44 +0700

    fix tutor 8

--- src/app/product/[[...slug]]/page.tsx
+++ src/app/product/[[...slug]]/page.tsx
@@ -1,9 +1,12 @@
 type ProductPage = { params: { slug: string[] } };
 
 async function getData() {
-  // const res = await fetch("https://fakestoreapi.com/products");
+  // const res = await fetch("https://fakestoreapi.com/products", {
+  //   cache: "no-store",
+  // });
+
   const res = await fetch("http://localhost:3000/api/product", {
-    cache: "force-cache",
+    cache: "no-store",
     next: {
       tags: ["products"],
     },
