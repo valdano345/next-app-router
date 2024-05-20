bca99b1f0ef04a34cecc69a89eaad1763906c07a
valdano <valdanoesnadiar@gmail.com>
2024-05-20 22:42:16 +0700

    fix tutor 13

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -1,4 +1,20 @@
+"use client";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 const DashboardPage = () => {
+  const { data: session, status }: { data: any; status: string } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    } else {
+      if (session !== undefined && session?.user.role !== "admin") {
+        router.push("/");
+      }
+    }
+  }, [router, session, session?.user.role, status]);
   return (
     <div>
       <h1 className="w-full h-96 bg-gray-300 rounded-[12px] flex justify-center items-center">
--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,9 +1,11 @@
+import { signIn, signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
 const Navbar = () => {
   const pathname = usePathname();
   const router = useRouter();
+  const { status }: { status: string } = useSession();
   return (
     <nav className="flex bg-gray-800 py-2 px-5 justify-between">
       <div className="flex">
@@ -39,12 +41,21 @@ const Navbar = () => {
         </ul>
       </div>
       <div>
-        <button
-          className="bg-white rounded-md px-3 text-sm h-7 cursor-pointer"
-          onClick={() => router.push("/login")}
-        >
-          Login
-        </button>
+        {status === "authenticated" ? (
+          <button
+            className="bg-white rounded-md px-3 text-sm h-7 cursor-pointer"
+            onClick={() => signOut()}
+          >
+            Logout
+          </button>
+        ) : (
+          <button
+            className="bg-white rounded-md px-3 text-sm h-7 cursor-pointer"
+            onClick={() => signIn()}
+          >
+            Login
+          </button>
+        )}
       </div>
     </nav>
   );
