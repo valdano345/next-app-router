6cdfc54ea7e4416f683740896b768c0126ded5b0
valdano <valdanoesnadiar@gmail.com>
2024-05-28 16:00:00 +0700

    fix tutor 14

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,42 @@
+"use client";
+
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const RegisterPage = () => {
+  const { push } = useRouter();
+  const [error, setError] = useState("");
+  const [isloading, setIsloading] = useState(false);
+  const handleSubmit = async (e: any) => {
+    setError("");
+    setIsloading(true);
+    e.preventDefault();
+    const res = await fetch("/api/auth/register", {
+      method: "POST",
+      body: JSON.stringify({
+        fullname: e.target.fullname.value,
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+    if (res.status === 200) {
+      e.target.reset();
+      setIsloading(false);
+      push("/login");
+    } else {
+      setError("Email Already Exists");
+      setIsloading(false);
+    }
+  };
+
   return (
-    <div className="h-screen w-100 flex justify-center items-center">
+    <div className="h-screen w-100 flex justify-center items-center flex-col">
+      {error !== "" && (
+        <div className="text-red-600 font-bold mb-3">{error}</div>
+      )}
       <div className="bg-white shadow-md border border-gray-200 rounded-lg max-w-sm p-4 sm:p-6 lg:p-8 dark:bg-gray-800 dark:border-gray-700">
-        <form className="space-y-6" action="#">
+        <form className="space-y-6" onSubmit={(e) => handleSubmit(e)}>
           <h3 className="text-xl font-medium text-gray-900 dark:text-white">
             Sign up to our platform
           </h3>
@@ -20,7 +52,7 @@ const RegisterPage = () => {
               name="fullname"
               id="fullname"
               className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-              placeholder="name@company.com"
+              placeholder="joend...."
               required
             />
           </div>
